73e5f6debfe0d9137b011e3e9fc516f6bad81791
Viktoria <vicky1234555617@gmail.com>
2023-11-09 19:08:20 +0000

    1st commit

---
+++ ex.py
@@ -0,0 +1,30 @@
+#print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+
+#values = input("Please input some comma separated values : ")
+#list = values.split(" , ")
+#tuple = tuple(list)
+#print('List : ', list)
+#print('Tuple: ', tuple)
+
+#color_list = ["Red", "Green", "Purple", "Yellow"]
+#color_list.remove[1,3]
+#print(color_list[0], color_list[-1])
+
+
+#from datetime import date
+
+#f_date = date(2014, 7, 2)
+#d_date = date(2014, 7, 11)
+
+#delta = d_date - f_date
+#print(delta)
+
+
+def difference(n):
+    if n<=17:
+        return 17 - n
+    else:
+        return (n-17) * 2
+    
+print(difference(22))
+print(difference(14))
---
+++ ex2.py
@@ -0,0 +1,81 @@
+#def larger_str(text, n):
+ #   result = " "
+  #  for i in range(n):
+   #
+   # result = result + text
+        
+        
+   # return result
+    
+#print(larger_str('admbc', 2))
+#print(larger_str('.py', 3))
+
+#def near_thousand(n):
+#    return((abs(1000-n) <=100) or (abs(2000 - n) <=100))
+
+#print(near_thousand(1000))
+#print(near_thousand(9000))
+#print(near_thousand(800))
+
+#num = int(input("Input) num : "))
+#mod = num % 2
+# if mod >0:
+#     print("Even")
+    
+# else:
+#     print("Odd ")
+
+
+# def list_count(nums):
+#     count = 0
+    
+#     for num in nums:
+      
+#       if num == 4:
+#         count = count + 1
+        
+#     return count
+
+# print(list_count[1, 3,4, 5])
+# print(list_count[1, 2, 4, 5 ,7, 4])
+        
+        # Define a function called list_count_4 that takes a list of numbers (nums) as a parameter.
+# 
+
+# def is_vowel(char):
+#     all_vowels = 'aeiou'
+    
+#     return char in all_vowels
+
+# print(is_vowel('c'))
+# print(is_vowel('e'))
+
+# numbers = [    
+#     386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+#     399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 
+#     815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 
+#     958,743, 527
+#     ]
+
+# for x in numbers:
+#     if x == 237:
+#         print(x)
+#         break
+#     elif x % 2 == 0:
+#         print(x)
+
+# color_list_1 = set(["White", "Black", "Red"])
+# color_list_2 = set(["Red", "Green"])
+
+# print(color_list_1)
+# print(color_list_2)
+
+# print("\nDifference of color lists are : ")
+# print(color_list_1.difference(color_list_2))
+
+
+# b = int(input("Enter the base num : "))
+# h = int(input("Input the height: "))
+
+# c = b / h * 2
+#print("area : ", c)
